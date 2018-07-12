11db1977df8e341aecb95595ae09bae5499a0c30
Bogdan Oniga <bogdan@oniga.me>
2018-07-12 14:37:13 +0300

    Setup small changes

--- setup.sh
+++ setup.sh
@@ -43,7 +43,7 @@ sudo apt-get install -y nodejs nginx sqlite3
 
 echo "[*] Installing lora_gateway"
 cd /usr/share/lora_gateway
-npm install -g
+sudo npm install -g
 
 echo "[*] Setting nginx config"
 sudo mv ./configs/nginx.conf /etc/nginx/sites-enabled/default
