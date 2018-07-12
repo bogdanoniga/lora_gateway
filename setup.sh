e9cbbce4eee7106120c88b4fcc21ae2cae6ea5a2
Bogdan Oniga <bogdan@oniga.me>
2018-07-12 14:33:23 +0300

    Setup small changes

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ sudo apt-get install -y apt-transport-https git make dirmngr
 
 echo "[*] Cloning lora_gateway software"
 cd /usr/share
-sudo git clone https://github.com/bogdanoniga/lora_gateway.git
-cd /usr/share/lora_gateway
+if [ ! -d "lora_gateway" ] ; then
+  sudo git clone https://github.com/bogdanoniga/lora_gateway.git
+fi
+sudo chmod -R 777 /usr/share/lora_gateway && cd /usr/share/lora_gateway
 
 echo "[*] Installing LoRa Gateway Bridge"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1CE2AFD36DBCCA00
@@ -19,26 +21,29 @@ sudo apt-get install -y lora-gateway-bridge
 sudo mv ./configs/lora-gateway-bridge.toml /etc/lora-gateway-bridge/lora-gateway-bridge.toml
 sudo chmod 777 /etc/lora-gateway-bridge/lora-gateway-bridge.toml
 
-echo "[*] Installing packet_forwarder, lora_gateway"
+echo "[*] Installing LoRa-Net lora_gateway"
 cd /usr/share/lora_gateway/
 sudo git clone https://github.com/Lora-net/lora_gateway.git
-cd /usr/share/lora_gateway/lora_gateway
+
+sudo chmod -R 777 /usr/share/lora_gateway/lora_gateway && cd /usr/share/lora_gateway/lora_gateway
 sed -i -e 's/DEBUG_HAL= 0/DEBUG_HAL= 1/g' ./libloragw/library.cfg
 sed -i -e 's/dtparam=spi=off/dtparam=spi=on/g' /boot/config.txt
 sudo make all
 
+echo "[*] Installing LoRa-Net packet_forwarder"
 cd /usr/share/lora_gateway/
 sudo git clone https://github.com/Lora-net/packet_forwarder.git
-cd /usr/share/lora_gateway/packet_forwarder
+
+sudo chmod -R 777 /usr/share/lora_gateway/packet_forwarder && cd /usr/share/lora_gateway/packet_forwarder
 sudo make all
 
 echo "[*] Installing Nodejs & npm"
 curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
 sudo apt-get install -y nodejs nginx sqlite3
 
-echo "[*] Installing lora_gateway "
-sudo chmod 777 /usr/share/lora_gateway && cd /usr/share/lora_gateway
-npm install
+echo "[*] Installing lora_gateway"
+cd /usr/share/lora_gateway
+npm install -g
 
 echo "[*] Setting nginx config"
 sudo mv ./configs/nginx.conf /etc/nginx/sites-enabled/default
@@ -54,3 +59,6 @@ sudo mv ./configs/packet_forwarder.sh /usr/local/bin && sudo chmod 744 /usr/loca
 
 echo "[*] Reloading systemctl"
 sudo systemctl daemon-reload
+
+echo "[*] Start LoRa gateway"
+sudo service lora_gateway start
